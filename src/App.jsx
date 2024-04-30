34ff105d56b59fdfdc741e3d099d2da41038872a
Prakash <127179011+ProPrak01@users.noreply.github.com>
2024-04-30 18:34:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+import Home from "./components/home"
+import Login from "./components/login"
+import Signup from "./components/signup"
+function App() {
+  
+  return (
+    <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/home" element={<Home/>}/>
+      </Routes>
+    </Router>
+  </div>
+  )
+}
+
+export default App
